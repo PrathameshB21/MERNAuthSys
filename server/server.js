815ee4c5a0cd7f6b572f120f04c477338c8303ac
PrathameshB21 <prathmeshborate21@gmail.com>
2025-05-26 14:22:39 +0530

    CORS Error resolved

--- server/server.js
+++ server/server.js
@@ -9,26 +9,32 @@ import userRoutes from './routes/userRoutes.js'
 import UserRouter from './routes/userDetailsRoutes.js'
 
 const app = express();
-
+const allowedOrigin = process.env.origin;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',   // ✅ Your frontend URL here
+    origin: function (allowedOrigin, callback) {
+        if (!allowedOrigin) {
+            callback(new Error('Blocked by CORS'))
+        } else {
+            callback(null, true)
+        }
+    },   // ✅ Your frontend URL here
     credentials: true                  // ✅ Allow credentials (cookies, headers)
 }));
-app.use('/userAuthentication',userRoutes);
-app.use('/userDetails',UserRouter);
+app.use('/userAuthentication', userRoutes);
+app.use('/userDetails', UserRouter);
 
-const port=process.env.Port||5000;
+const port = process.env.Port || 5000;
 connectDB()
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Api working")
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`--------------🚀 Server running on port ${port}--------------`);
-    
+
 });
 
 
